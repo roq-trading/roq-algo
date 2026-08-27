88685397cb05ea6cd8e4aa1228f4a1b2193b31f1
thraneh <thraneh@gmail.com>
2026-08-27 06:03:08 +0000

    chore

--- src/roq/algo/arbitrage/simple.cpp
+++ src/roq/algo/arbitrage/simple.cpp
@@ -86,11 +86,11 @@ auto create_sources(auto &instruments) {
 
 // === IMPLEMENTATION ===
 
-Simple::Simple(Dispatcher &dispatcher, OrderCache &order_cache, strategy::Config const &config, Parameters const &parameters)
+Simple::Simple(Dispatcher &dispatcher, OrderCache &, strategy::Config const &config, Parameters const &parameters)
     : dispatcher_{dispatcher}, strategy_id_{config.strategy_id}, max_age_{parameters.max_age}, threshold_{parameters.threshold},
-      quantity_0_{parameters.quantity_0}, min_position_0_{parameters.min_position_0}, max_position_0_{parameters.max_position_0},
-      publish_source_{parameters.publish_source}, market_data_type_{create_market_data_type(parameters)}, order_cache_{order_cache},
-      instruments_{create_instruments<decltype(instruments_)>(config, parameters)}, sources_{create_sources<decltype(sources_)>(instruments_)} {
+      min_position_0_{parameters.min_position_0}, max_position_0_{parameters.max_position_0}, publish_source_{parameters.publish_source},
+      market_data_type_{create_market_data_type(parameters)}, instruments_{create_instruments<decltype(instruments_)>(config, parameters)},
+      sources_{create_sources<decltype(sources_)>(instruments_)} {
   assert(!std::empty(instruments_));
   assert(!std::empty(sources_));
 }
